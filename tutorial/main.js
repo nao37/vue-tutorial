a0444a3947b00640c88fceadc0c7295cd506da7b
渡辺直 <w.nao.gh37@gmail.com>
2019-07-12 16:25:12 +0900

    fix

--- tutorial/main.js
+++ tutorial/main.js
@@ -3,10 +3,22 @@ var app = new Vue({
   methods: {
     handleClick: function (event) {
       alert(event.target)
+    },
+    increment: function () {
+      this.count++
     }
   },
   data: {
     message: '初期メッセージ',
     show: true,
+    scroll: 0,
+    count: 0,
+    isChild: true,
+    isActive: true,
+    textColor: "red",
+    bgColor: "yellow",
+  },
+  mounted: function () {
+    this.scroll = 100
   }
 })
