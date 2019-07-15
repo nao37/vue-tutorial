56ad71df923d696e545c0fa9c29600b23ecdeb1d
渡辺直 <w.nao.gh37@gmail.com>
2019-07-15 16:39:51 +0900

    fix

--- guide/main.js
+++ guide/main.js
@@ -1,3 +1,10 @@
+Vue.component('todo-item', {
+  // todo-item コンポーネントはカスタム属性のような "プロパティ" で受け取ります。
+  // このプロパティは todo と呼ばれます。
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
 var app = new Vue({
   el: "#app",
   data: {
@@ -9,5 +16,15 @@ var app2 = new Vue({
   el: "#app2",
   data: {
     message: 'You loaded this page on ' + new Date().toLocaleString(),
+    groceryList: [{
+      id: 1,
+      text: "Vegetables",
+    }, {
+      id: 2,
+      text: "Cheese",
+    }, {
+      id: 3,
+      text: "Whatever else humans are supposed to eat",
+    }, ]
   }
 })
