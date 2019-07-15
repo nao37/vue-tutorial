d447fa82e29a6112c064f26530d61e5ddcf731fe
渡辺直 <w.nao.gh37@gmail.com>
2019-07-15 23:35:15 +0900

    fix

--- guide/main.js
+++ guide/main.js
@@ -5,11 +5,45 @@ Vue.component('todo-item', {
   template: '<li>{{ todo.text }}</li>'
 })
 
+Vue.component('todo-item', {
+  template: '<li>{{ title }}<button v-on:click="$emit(\'remove\')">Remove</button></li>',
+  props: ['title'],
+})
+
 var app = new Vue({
   el: "#app",
   data: {
     message: 'Hello Vue!',
+  }
+})
+
+new Vue({
+  el: "#todo-list-example",
+  data: {
+    todos: [{
+        id: 1,
+        title: 'Do the dishes',
+      },
+      {
+        id: 2,
+        title: 'Take out the trash',
+      },
+      {
+        id: 3,
+        title: 'Mow the lawn'
+      }
+    ],
+    newTodoText: "",
+    nextTodoId: 4,
   },
+  methods: {
+    addNewTodo: function () {
+      this.todos.push({
+        id: this.nextTodoId++,
+        title: this.newTodoText,
+      })
+    },
+  }
 })
 
 var app2 = new Vue({
