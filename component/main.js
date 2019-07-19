b50f95459628117095fbbb05cab85419f991f7b6
渡辺直 <w.nao.gh37@gmail.com>
2019-07-19 19:06:37 +0900

    fix

--- component/main.js
+++ component/main.js
@@ -4,9 +4,17 @@ Vue.component('blog-post', {
 })
 
 Vue.component('comp-child', {
-  props: [],
-  data: {},
-  template: '',
+  props: {
+    id: Number,
+    name: String,
+    hp: Number,
+  },
+  template: '<li>{{ name }} HP.{{ hp }}<button v-on:click="doAttack">攻撃する</button></li>',
+  methods: {
+    doAttack: function () {
+      this.$emit('attack', this.id)
+    }
+  },
 })
 
 var app = new Vue({
@@ -15,6 +23,32 @@ var app = new Vue({
     book: {
       'title': 'naoko',
       'author': 'naoki',
+    },
+    items: [{
+        id: 1,
+        name: "nao",
+        hp: 100
+      },
+      {
+        id: 2,
+        name: "taro",
+        hp: 200
+      },
+      {
+        id: 3,
+        name: "hanako",
+        hp: 300
+      },
+    ]
+  },
+  methods: {
+    handleAttack: function (id) {
+      var item = this.items.find(function (el) {
+        return el.id === id
+      })
+      if (item.hp !== undefined && item.hp > 0) {
+        item.hp -= 10
+      }
     }
   },
   components: {
